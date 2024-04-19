6f9e44c926d4c75d2ab4e2b262d4a59df493d3be
kamiliskhakov <kamilisxakof@gmail.com>
2024-04-19 10:55:48 +0300

    очень мелкие правки, кроме help

--- src/main/java/Client/Main.java
+++ src/main/java/Client/Main.java
@@ -10,7 +10,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 import Service.*;
-
+//не забудь исправить help
 public class Main {
     public static boolean script = false;
     public static TerminalOutputManager terminalOutputManager;
